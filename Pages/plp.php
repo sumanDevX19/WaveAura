2eab5c9c21aa90d1aa761a4c5e327a1bcc97c248
sumanDevX19 <contactmrsumansaha@gmail.com>
2025-08-04 22:07:57 +0530

    04/08/2025: token time increase for 30 mins, Edited the product.php endpoint for accessing number of products at a time, some new image added to the resource folder, remove some unnessessary pary from index.php, plp page added, some change in js scripts productFetching, plp css addded

---
+++ Pages/plp.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="author" content="Suman Saha" />
+  <meta
+    name="keywords"
+    content="Headphone Store, E-commerce, E-comm Store, Online Shopping" />
+  <meta
+    name="description"
+    content="WaveAura is a Online store of Headsets and Headphone at best price." />
+
+  <title>WaveAura</title>
+  <link rel="stylesheet" href="../Stylesheets/plpstyle.css" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+    integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer" />
+  <link
+    rel="shortcut icon"
+    href="../headphone_resources/Favicon/favicon.ico"
+    type="image/x-icon" />
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=shopping_cart" />
+</head>
+
+<body>
+    <?php
+    session_start();
+    ?>
+    <header class="head-section">
+    <div class="head-left">
+      <h2>WaveAura</h2>
+    </div>
+    <nav class="head-right">
+      <a href="http://localhost/WaveAura/Pages/index.php" class="navlink">Home</a>
+      <a href="http://localhost/WaveAura/Pages/index.php#hero" class="navlink" id="store">Store</a>
+      <a href="#" class="navlink">About</a>
+      <a href="#" class="navlink">Contact</a>
+
+
+      <?php
+      session_start();
+      $fullName = $_SESSION['user_name'] ?? 'Guest';
+      $nameParts = explode(" ", $fullName);
+      $firstName = $nameParts[0] ?? 'Guest';
+
+
+      ?>
+
+      <script>
+        const jwt = "<?php echo $_SESSION['jwt'] ?? ''; ?>";
+        console.log(jwt);
+      </script>
+
+
+
+
+      <a href="#" class="navlink" id="user-container">Welcome, <?= $firstName ?>!</a>
+
+
+      <?php if (isset($_SESSION['jwt'])): ?>
+        <a href="../Pages/logout.php" class="navlink">Logout</a>
+      <?php else: ?>
+        <a href="../Pages/Login.html" class="navlink">Login</a>
+      <?php endif; ?>
+      <a href="cart.php" class="navlink"><span class="material-symbols-outlined" style="cursor: pointer;">
+          shopping_cart
+        </span></a>
+
+      <!-- <a href="" class="navlink" id="usericon"><i class="fa-solid fa-circle-user fa-2xl"></i></a> -->
+      <div class="navmenu">
+        <div class="navmenu-container">
+          <div class="navmenu-column">
+            <h5 class="heading-menu">Professional Headset</h5>
+            <a href="">Sony Headset</a>
+            <a href="">Samsung Headset</a>
+            <a href="">Apple Headset</a>
+            <a href="">Headset Under 40000/-</a>
+            <a href="">Headset Under 30000/-</a>
+            <a href="">Headset Under 20000/-</a>
+            <a href="">Headset Under 10000/-</a>
+          </div>
+          <div class="navmenu-column">
+            <h5 class="heading-menu">Gaming Headset</h5>
+            <a href="">Sony Headset</a>
+            <a href="">Samsung Headset</a>
+            <a href="">Apple Headset</a>
+            <a href="">Headset Under 40000/-</a>
+            <a href="">Headset Under 30000/-</a>
+            <a href="">Headset Under 20000/-</a>
+            <a href="">Headset Under 10000/-</a>
+          </div>
+          <div class="navmenu-column">
+            <h5 class="heading-menu">Production Headset</h5>
+            <a href="">Sony Headset</a>
+            <a href="">Samsung Headset</a>
+            <a href="">Apple Headset</a>
+            <a href="">Headset Under 40000/-</a>
+            <a href="">Headset Under 30000/-</a>
+            <a href="">Headset Under 20000/-</a>
+            <a href="">Headset Under 10000/-</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <a class="navlink" id="menu-icon"><i class="fa-solid fa-bars fa-2xl"></i></a>
+
+    <div class="responsive-menu">
+      <a href="http://localhost/WaveAura/Pages/index.php" class="responsive-menu-icon">Home</a>
+      <a href="" class="responsive-menu-icon">Store</a>
+      <a href="" class="responsive-menu-icon">About</a>
+      <a href="" class="responsive-menu-icon">Contact</a>
+      <a href="" class="responsive-menu-icon">Sign Up</a>
+
+
+    </div>
+    
+  </header>
+
+  <main>
+    <div class="main-container">
+      <div id="toast" class="toast">
+        <span id="toast-message"></span>
+      </div>
+      <section class="product-category-section" id="hero">
+      <h2 class="product-catagory-heading">Headphone Categories</h2>
+      <div class="product-categories" id="category-area">
+        <!-- Data is Comming from JS -->
+      </div>
+    </section>
+    </div>
+     <section class="product-gallery" id="product-gallery">
+      <h2 class="product-gallary-heading">Top Trending Products</h2>
+      <div class="product-lineup">
+        <!-- Data is Coming from js -->
+
+
+
+      </div>
+    </section>
+
+
+  </main>
+
+
+  <footer class="footer">
+    <div class="footer-container">
+      <div class="footer-column">
+        <h3>About Us</h3>
+        <p>
+          Your go-to store for premium headphones. Experience sound like never
+          before.
+        </p>
+      </div>
+      <div class="footer-column">
+        <h3>Quick Links</h3>
+        <ul>
+          <li><a href="/shop">Shop</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/faq">FAQ</a></li>
+        </ul>
+      </div>
+      <div class="footer-column">
+        <h3>Contact</h3>
+        <p>Email: support@yourstore.com</p>
+        <p>Phone: +1 (234) 567-890</p>
+      </div>
+      <div class="footer-column social">
+        <h3>Follow Us</h3>
+        <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+        <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <p>&copy; 2025 WaveAura. All rights reserved.</p>
+    </div>
+  </footer>
+
+<script src="../Scripts/menutoggler.js"></script>
+<script src="../Scripts/productFetchingPlp.js"></script>
+<script>
+    const category = document.querySelector("#category-area");
+    console.log(category);
+    category.innerHTML = "";
+
+    async function getCategoryData() {
+
+      try {
+        const response = await fetch(`http://localhost/WaveAura/Backend/category.php`);
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+
+        const data = await response.json();
+        return data;
+      } catch (err) {
+        console.error("Something Went Wrong", err);
+      }
+    }
+
+    async function renderCategory() {
+      let data = await getCategoryData();
+
+      console.log(data);
+
+
+      data.forEach((cate) => {
+        const catDiv = document.createElement("div");
+        catDiv.classList.add("product");
+
+        catDiv.innerHTML = `
+            <a href="#">
+            <figure class="menu-back">
+            
+              <img
+                src="${cate.imgsrc}"
+                class="menu-image"
+                alt="${cate.name} image"
+          />
+          
+        </figure>
+        </a>
+        <h3>${cate.name}</h3>
+        `;
+
+        category.appendChild(catDiv);
+
+
+      })
+    }
+
+    renderCategory()
+  </script>
+
+
+</body>
+
+</html>
